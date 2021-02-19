3a89c885ff3e4be4452e32038bfbd175e1187754
Felix Frank <felix@street14.de>
2021-02-19 12:49:51 +0100

    first draft for writeAudio of spencer device (not working properly yet)

--- PlatformIO/src/devices/SpencerDevice.cpp
+++ PlatformIO/src/devices/SpencerDevice.cpp
@@ -5,6 +5,14 @@
 #include "AudioFileSourcePROGMEM.h"
 
 
+#define CONFIG_I2S_BCK_PIN 16
+#define CONFIG_I2S_LRCK_PIN 27
+#define CONFIG_I2S_DATA_PIN 4
+#define CONFIG_I2S_DATA_IN_PIN 32
+
+#define SPEAKER_I2S_NUMBER I2S_NUM_0
+
+
 const uint32_t i2sBufferSize = 1600;
 
 struct SpencerDevice::SpencerDeviceImpl {
@@ -20,6 +28,8 @@ struct SpencerDevice::SpencerDeviceImpl {
 
     //AudioGeneratorWAV wav;
     //AudioOutputI2S out;
+
+    int sampleRate, bitDepth, numChannels;
 };
 
 
@@ -52,20 +62,19 @@ void SpencerDevice::init()
 
 void SpencerDevice::setWriteMode(int sampleRate, int bitDepth, int numChannels)
 {
+    impl->sampleRate = sampleRate;
+	impl->bitDepth = bitDepth;
+	impl->numChannels = numChannels;
     const int modeSpeaker = 1;
     if (mode != modeSpeaker)
     {
         Serial.println("Init speaker");
         impl->InitI2SSpeakerOrMic(modeSpeaker);
-        //impl->out.SetChannels(numChannels);
-        //impl->out.SetRate(sampleRate);
-        //impl->out.SetBitsPerSample(bitDepth);
-
-        if (sampleRate > 0) {
-            i2s_set_clk(I2S_NUM_0, sampleRate, static_cast<i2s_bits_per_sample_t>(bitDepth), static_cast<i2s_channel_t>(numChannels));
-        }
-
         mode = modeSpeaker;
+    }
+
+    if (sampleRate > 0) {
+        i2s_set_clk(I2S_NUM_0, sampleRate / 2, static_cast<i2s_bits_per_sample_t>(2*bitDepth), static_cast<i2s_channel_t>(numChannels));
     }
 }
 
@@ -80,38 +89,120 @@ void SpencerDevice::setReadMode()
     }
 }
 
+// void interleave(const int16_t * in_L, const int16_t * in_R, int16_t * out, const size_t num_samples)
+// {
+//     for (size_t i = 0; i < num_samples; ++i)
+//     {
+//         out[i * 2] = in_L[i];
+//         out[i * 2 + 1] = in_R[i];
+//     }
+// }
+
 void SpencerDevice::writeAudio(uint8_t *data, size_t size, size_t *bytes_written)
 {
-    /**
-    // Setup
-	AudioFileSourcePROGMEM audioSource(data, size);
-	// AudioGeneratorWAV wav;
-	// AudioOutputI2S out(0,0,16,0);
-	// out.SetRate(16000);
-	// out.SetPinout(16, 27, 4);
-	// out.SetChannels(1);
-	// out.SetOutputModeMono(1);
-	// float volume = 0.4;
-	// out.SetGain(volume);
+    //Serial.print("Size: ");
+    //Serial.println(size);
+    // if( impl->numChannels == 2 ){
+    //     int16_t mono[size / sizeof(int16_t)];
+    //     // Convert 8 bit to 16 bit
+    //     for (int i = 0; i < size; i += 2) {
+    //         mono[i/2] = ((data[i] & 0xff) | (data[i + 1] << 8));
+    //     }
 
-	// Run
-	Serial.println("Start playback from data");
-	impl->wav.begin(&audioSource, &impl->out);
-	if(impl->wav.isRunning()){
-		impl->wav.loop();
-	}
-	Serial.println("End playback from data");
 
-    *bytes_written = size;
-    */
+    //     // const uint32_t bufferSize16bit = sizeof(uint16_t) * size / 4;
+    //     // uint16_t buffer16bit[bufferSize16bit];
+    //     // for(int j = 0; j < size; j += 4){
+    //     //     uint16_t sample = *(uint16_t*) (&data[j + 2]) /*+ 3705*/;
+    //     //     buffer16bit[j / 4] = sample;
+    //     // }
 
-   i2s_write(I2S_NUM_0, data, size, bytes_written, portMAX_DELAY);
+
+
+    //     // int16_t mono[size / sizeof(int16_t)];
+    //     // // Convert 8 bit to 16 bit
+    //     // for (int i = 0; i < size; i += 2) {
+    //     //     mono[i/2] = data[i];
+    //     // }
+
+
+
+    //     // int16_t mono[size / sizeof(int16_t)];
+    //     // int16_t stereo[size];
+    //     // //Convert 8 bit to 16 bit
+    //     // for (int i = 0; i < size; i += 2) {
+    //     //     mono[i/2] = ((data[i] & 0xff) | (data[i + 1] << 8));
+    //     // }
+    //     // interleave(mono, mono, stereo, size / sizeof(int16_t));
+
+
+    //     // int16_t mono[size*sizeof(uint16_t)];
+    //     // //Convert 8 bit to 16 bit
+    //     // for (int i = 0; i < size; ++i) {
+    //     //     int16_t sample = data[i];
+    //     //     mono[i] = sample * 256;
+    //     // }
+
+
+    //     i2s_write(I2S_NUM_0, mono, size, bytes_written, portMAX_DELAY);
+    // }
+    // else
+    // {
+    //     int16_t mono[size / sizeof(int16_t)];
+    //     //Convert 8 bit to 16 bit
+    //     for (int i = 0; i < size; i += 2) {
+    //         mono[i/2] = ((data[i] & 0xff) | (data[i + 1] << 8));
+    //     }
+    //     i2s_write(I2S_NUM_0, mono, sizeof(mono), bytes_written, portMAX_DELAY);
+    // }
+
+
+
+    i2s_write(I2S_NUM_0, data, size, bytes_written, portMAX_DELAY);
 }
 
 void SpencerDevice::SpencerDeviceImpl::InitI2SSpeakerOrMic(int mode)
 {
-    i2s.stop();
-    i2s.begin();
+    // use MODE_MIC from deviceDefinitions
+    int MODE_MIC = 0;
+
+    esp_err_t err = ESP_OK;
+
+    i2s_driver_uninstall(SPEAKER_I2S_NUMBER);
+    i2s_config_t i2s_config = {
+        .mode = (i2s_mode_t)(I2S_MODE_MASTER),
+        .sample_rate = 16000,
+        .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT, // spencer library says: could only get it to work with 32bits
+        .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+        .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S | I2S_COMM_FORMAT_I2S_MSB),
+        .intr_alloc_flags = 0, // Interrupt level 1
+        .dma_buf_count = 16,
+        .dma_buf_len = 60,
+    };
+    if (mode == MODE_MIC)
+    {
+        i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX);
+        i2s_config.use_apll = false;
+    }
+    else
+    {
+        i2s_config.mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX);
+        i2s_config.use_apll = false;
+        i2s_config.tx_desc_auto_clear = true;
+    }
+
+    err += i2s_driver_install(SPEAKER_I2S_NUMBER, &i2s_config, 0, NULL);
+    i2s_pin_config_t tx_pin_config;
+
+    tx_pin_config.bck_io_num = CONFIG_I2S_BCK_PIN;
+    tx_pin_config.ws_io_num = CONFIG_I2S_LRCK_PIN;
+    tx_pin_config.data_out_num = CONFIG_I2S_DATA_PIN;
+    tx_pin_config.data_in_num = CONFIG_I2S_DATA_IN_PIN;
+
+    err += i2s_set_pin(SPEAKER_I2S_NUMBER, &tx_pin_config);
+    err += i2s_set_clk(SPEAKER_I2S_NUMBER, 16000, I2S_BITS_PER_SAMPLE_32BIT, I2S_CHANNEL_MONO);
+
+    return;
 }
 
 bool SpencerDevice::readAudio(uint8_t *data, size_t size)
